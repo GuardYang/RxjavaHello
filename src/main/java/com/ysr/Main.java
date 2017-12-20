1570bac0495ae4a7197241df452af6aeccc1e5d8
yangshirong <yangshirong@log56.com>
2017-12-20 17:47:30 +0800

    hello

--- src/main/java/com/ysr/Main.java
+++ src/main/java/com/ysr/Main.java
@@ -1,12 +1,43 @@
 package com.ysr;
 
-import io.reactivex.Flowable;
-import io.reactivex.Observable;
+
+import rx.Observable;
+import rx.Subscriber;
 
 public class Main {
 
     public static void main(String[] args) {
-        Flowable.just("Hello world").subscribe(System.out::println);
+        //创建被观察者
+        Observable observable = Observable.create(new Observable.OnSubscribe<String>() {
 
+            @Override
+            public void call(Subscriber<? super String> subscriber) {
+                subscriber.onNext("call");
+                subscriber.onCompleted();
+//                throw new NullPointerException();
+            }
+        });
+        //创建观察者
+        /**
+         * onCompleted he onError 单联
+         */
+        Subscriber subscriber = new Subscriber<String>() {
+            @Override
+            public void onCompleted() {
+                System.out.println("onCompleted");
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println("onError");
+            }
+
+            @Override
+            public void onNext(String o) {
+                System.out.println("onNext>>"+o);
+            }
+        };
+        //订阅
+        observable.subscribe(subscriber);
     }
 }
